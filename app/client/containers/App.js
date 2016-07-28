280708ccfb1b797a728597b1c9f3a730b6195cda
Albin <j.albin.willman@gmail.com>
2016-07-28 21:47:06 +0200

    Adds mor info in footer

--- app/client/containers/App.js
+++ app/client/containers/App.js
@@ -54,10 +54,11 @@ export default class App extends React.Component {
       <Grid style={STYLES.grid}>
         {content}
         <p>
-          * All positions below 0.5% are considered to be 0%.<br/>
-          ** Data updated {updated}<br/>
-          *** Data from <a target="_blank" href="http://fi.se/Register/Blankning/" >Finans inspektionen</a><br/>
-          **** Feature requests accepted via <a target="_blank" href="https://twitter.com/SvenssonAlbin" >@SvenssonAlbin</a><br/>
+          All positions below 0.5% are considered to be 0%.<br/>
+          Data updated {updated}<br/>
+          Data from <a target="_blank" href="http://fi.se/Register/Blankning/" >Finans inspektionen</a><br/>
+          Feature requests accepted via <a target="_blank" href="https://twitter.com/SvenssonAlbin" >@SvenssonAlbin</a><br/>
+          Source code available at <a target="_blank" href="https://github.com/Albin-Willman/short-tracker/" >Github</a>
 
         </p>
       </Grid>
