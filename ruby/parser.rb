d0a7aac02417246d01251dd2f108d7419b3995bf
Albin <j.albin.willman@gmail.com>
2016-08-22 22:10:10 +0200

    Fixes ruby issues

--- ruby/parser.rb
+++ ruby/parser.rb
@@ -26,7 +26,7 @@ class XlsParser
     company = find_company_key(company_name)
 
     amount = file.cell(line, 5)
-    amount = amount.gsub(',', '.').to_f if amount.is_a?(String)
+    amount = amount.tr(',', '.').to_f if amount.is_a?(String)
     amount = 0 if amount <= 0.5
     date = Date.parse(file.cell(line, 6).to_s)
     company(company, company_name, date)
